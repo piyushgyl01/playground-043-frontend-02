ff509039842f517d5053c5d45db9709ce85b9d37
piyush.gyl <piyush2022ug@gmail.com>
2025-04-22 04:05:31 +0530

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import Post from "./pages/Post";
+import Update from "./pages/Update";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/update/:id" element={<Update />} />
+      </Routes>
+    </Router>
+  );
 }
