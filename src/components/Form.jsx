8267304691faa7033c2d94d1f637695cd77e9c3b
piyush.gyl <piyush2022ug@gmail.com>
2025-04-22 04:39:44 +0530

    feat: post & edit pages

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,184 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import useFetch from "../hooks/useFetch";
+import { Link } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default function Form() {
+export default function Form({ isEdit }) {
+  const [formData, setFormData] = useState({
+    coverURL: "",
+    title: "",
+    description: "",
+    author: "",
+  });
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data, loading, error, refetch } = useFetch(
+    `${import.meta.env.VITE_API_URL}/manhwas/${id}`
+  );
+  console.log(data);
+
+  useEffect(() => {
+    if (isEdit && data) {
+      setFormData({
+        coverURL: data?.coverURL || "",
+        title: data?.title || "",
+        description: data?.description || "",
+        author: data?.author || "",
+      });
+    }
+  }, [isEdit, id, data]);
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    try {
+      if (isEdit) {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/manhwas/${id}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+
+        if (response.ok) {
+          navigate(`/details/${id}`);
+          refetch();
+        }
+      } else {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/manhwas`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+
+        if (response.ok) {
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <div>Form</div>
-  )
+    <>
+      {loading ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="card shadow border-0">
+          <div className="card-body p-4">
+            <h2 className="card-title p-4">
+              {isEdit ? "Edit Manhwa Details" : "Add New Manhwa"}
+            </h2>
+
+            {error && (
+              <div className="d-flex justify-content-center align-items-center">
+                <div className="alert alert-danger">{error}</div>
+              </div>
+            )}
+
+            <form onSubmit={onSubmit}>
+              <div className="mb-4">
+                <label htmlFor="coverURL" className="form-label">
+                  Cover URL
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="coverURL"
+                  id="coverURL"
+                  value={formData.coverURL}
+                  onChange={(e) =>
+                    setFormData({ ...formData, coverURL: e.target.value })
+                  }
+                  required
+                  placeholder="https://placehold.co/600x400"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label htmlFor="title" className="form-label">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="title"
+                  id="title"
+                  value={formData.title}
+                  onChange={(e) =>
+                    setFormData({ ...formData, title: e.target.value })
+                  }
+                  required
+                  placeholder="One Piece"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label htmlFor="description" className="form-label">
+                  Description
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="description"
+                  id="description"
+                  value={formData.description}
+                  onChange={(e) =>
+                    setFormData({ ...formData, description: e.target.value })
+                  }
+                  required
+                  placeholder="A world of pirates"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label htmlFor="author" className="form-label">
+                  Author
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="author"
+                  id="author"
+                  value={formData.author}
+                  onChange={(e) =>
+                    setFormData({ ...formData, author: e.target.value })
+                  }
+                  required
+                  placeholder="Oda"
+                />
+              </div>
+
+              <div className="d-flex gap-2">
+                <Link
+                  to={isEdit ? `/details/${id}` : "/"}
+                  className="btn btn-secondary"
+                >
+                  Cancel
+                </Link>
+                <button type="submit" className="btn btn-primary">
+                  {loading ? "Loading" : isEdit ? "Save Changes" : "Add Anime"}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
